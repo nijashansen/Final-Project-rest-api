a61ae798470f0d62b9281c6c58376f4cfe0fc7fe
nijashansen <nijas.w.hansen@gmail.com>
2020-11-05 19:57:46 +0100

    yay

--- FinalProjectAPI/Controllers/ErrorController.cs
+++ FinalProjectAPI/Controllers/ErrorController.cs
@@ -30,25 +30,47 @@ namespace FinalProjectAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<Error> Get(int id)
         {
+            if (id < 0)
+            {
+                return BadRequest("id must be greater than 0");
+            }
             return _errorService.FindErrorById(id);
         }
 
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Error> Post([FromBody] Error error)
         {
-
+            if (string.IsNullOrEmpty(error.ErrorDetail))
+            {
+                return BadRequest("error detail must not be empty or null");
+            }
+            else if (string.IsNullOrEmpty(error.ErrorType))
+            {
+                return BadRequest("error type must not be empty or null");
+            }
+            return _errorService.CreateError(error);
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Error> Put(int id, [FromBody] Error error)
         {
-
+            if (id < 1 || id != error.Id)
+            {
+                return BadRequest("parameter id and error id was not the same");
+            }
+            
+            return Ok(_errorService.UpdateError(error));
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Error> Delete(int id)
         {
-            _errorService.DeleteError(id);
+            var error = _errorService.DeleteError(id);
+            if (error == null)
+            {
+                return StatusCode(404, "did not find error with: " + id);
+            }
+            return Ok($"Error with id {id} is Deleted");
         }
     }
 }
